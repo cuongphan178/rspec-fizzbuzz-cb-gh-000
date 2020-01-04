5f81cdff97e2a936d82a0f8ea7f5970c1ab8460e
Cuong Phan <at120906@actvn.edu.vn>
2020-01-04 16:12:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elsif int % == 5
+  elsif int % 5 == 0
     "Buzz"
-  elsif int % == 3 && int % == 5
+  elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   else
     nil
